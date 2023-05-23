9fb774f9ce8e9936553dfc026342ebc9718f20ed
Kauã Landi <eu@kaualf.com>
2023-05-23 11:38:38 -0300

    feat: add log in receive message

--- src/models/receive-message/receive-message.service.ts
+++ src/models/receive-message/receive-message.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { ReceiveMessageDto } from './dto/receive-message.dto';
-import fs from 'fs';
 
 @Injectable()
 export class ReceiveMessageService {
@@ -10,11 +9,6 @@ export class ReceiveMessageService {
     receiveMessageDto?.entry.forEach((entry) => {
       entry.changes.forEach((messagingItem) => {
         console.log('messagingItem', messagingItem);
-
-        fs.writeFileSync(
-          `./${messagingItem.metadata.phone_number_id}.json`,
-          JSON.stringify(messagingItem, null, 2),
-        );
 
         console.log('\n\nmessagingItem.contacts', messagingItem.contacts);
         messagingItem.contacts?.forEach((contact) => {
